bf09d96b198d1ad201610ca35e5504b6824419d0
Drew Cross <mstngdrew212@gmail.com>
2012-03-31 13:11:12 -0500

    Added test suite and empty server tests - #5

---
+++ test/random_tests.py
@@ -0,0 +1,11 @@
+import unittest
+
+class RandomTests(unittest.TestCase):
+    """
+    Example test file to show how to add unit tests to the test suite
+    """
+    def setUp(self):
+        pass
+
+    def test_random(self):
+        self.assertTrue(False)
