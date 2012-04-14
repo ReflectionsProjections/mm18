760296bcc7932f6e9d925132aae1647523bd7c5c
Calvin Shirley <calvin@calvinshirley.net>
2012-04-14 16:02:46 -0500

    Initialize logging function

--- mm18/server/run.py
+++ mm18/server/run.py
@@ -3,6 +3,13 @@
 import server
 
 def Main():
+	
+	import logging
+	# Starts logging function
+	logging.basicConfig(filename='Server.log',level=logging.DEBUG)
+	logging.debug('Logging function started')
+	# Sets logging level - available options are DEBUG, INFO, WARNING, ERROR, CRITICAL
+	
 	serve = server.ThreadedHTTPServer(('localhost', 6969), server.MMHandler)
 	# This prevents errors where the socket is still bound
 	serve.allow_reuse_address = True
