e7665dde944890bf0ddfe33ccb157c1fc8267859
Calvin Shirley <calvin@calvinshirley.net>
2012-04-16 00:26:40 -0500

    Made the logging code more user-friendly

--- mm18/server/run.py
+++ mm18/server/run.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python
 
 import server
+import logging
 
 def Main():
-
-	import logging	
-	# Starts logging function
-	logging.basicConfig(format='%(levelname)s: %(message)s',filename='Server.log',level=logging.DEBUG)
-	logging.info('Logging function started')
-	# Sets logging level - available options are DEBUG, INFO, WARNING, ERROR, CRITICAL
 	
+	# LOG SETTINGS
+	# log_file - the filename to output logging to
+	# log_format - format to write log messages in
+	# log_level - threshold for logging. Options are logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR, logging.CRITICAL
+	
+	log_file = 'Server.log'
+	log_format = '%(levelname)s: %(message)s'	
+	log_level = "logging.INFO"
+
+	# Starts logging function
+	logging.basicConfig(format='%(levelname)s: %(message)s',filename='Server.log',level=log_level)
+	logging.info('Logging function started')
+	
+
 	serve = server.ThreadedHTTPServer(('localhost', 6969), server.MMHandler)
 	# This prevents errors where the socket is still bound
 	serve.allow_reuse_address = True
