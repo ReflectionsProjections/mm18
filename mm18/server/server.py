eb843e2eaa78b9abae2f019dd8038551abda508a
Dylan Nugent <dylnuge@gmail.com>
2012-05-15 11:24:25 -0500

    Improving server.py style to meet standards
    I want server.py to be a shining beacon of code for the world to look at
    in awe and wonder. Or at least for our dev team to be able to see how
    code should look. If that means I spend the day styling it, so be it.

--- mm18/server/server.py
+++ mm18/server/server.py
@@ -16,6 +16,7 @@ class MMHandler(BaseHTTPRequestHandler):
 		status_code -- string containting HTTP status code.
 		data -- dictionary to encode to JSON
 		"""
+
 		self.send_response(int(status_code))
 		output = json.dumps(data)
 		self.send_header("Content-type", "application/json")
@@ -24,27 +25,32 @@ class MMHandler(BaseHTTPRequestHandler):
 
 	def match_path(self, method):
 		"""Tries to match a path with every url in urlpatterns.
-		If it finds one, it tries to call it.  Then it breaks out, 
-		so it will only call the first pattern it matches.  This method
-		calls the corresponding function in urlpatterns from urls.py, so
-		expect side effects from the game controller.  It also handles
-		serializtion of JSON and will send a 400 error if given invalid
-		JSON.  Wil send a 404 if no matching URL is found and a 405 if 
-		a URL is found but with the wrong method.
 
-		method - string containing the HTTP request method
+		If it finds one, it tries to call it.  Then it breaks out, so it will
+		only call the first pattern it matches.  This method calls the
+		corresponding function in urlpatterns from urls.py, so expect side
+		effects from the game controller.  It also handles serializtion of JSON
+		and will send a 400 error if given invalid JSON.  Wil send a 404 if no
+		matching URL is found and a 405 if a URL is found but with the wrong
+		method.
+
+		method -- string containing the HTTP request method
 		"""
+
 		matched_url = False
 		invalid_method = False
 		invalid_JSON = False
+
 		for url in urlpatterns:
 			match = re.match(url[0], self.path)
+
 			# check if match is found
 			if match:
 				# if method is same, process
 				if method == url[1]:
-					# POST data should be catured
+
 					if method == 'POST':
+						# POST data should be catured
 						try:
 							length = int(self.headers['Content-Length'])
 							input = self.rfile.read(length)
@@ -52,12 +58,14 @@ class MMHandler(BaseHTTPRequestHandler):
 						except ValueError:
 							# invalid JSON, send 400 error
 							invalid_JSON = True
-					# GET method, so empty dictionary
+
 					else:
+						# GET method, so empty dictionary
 						data = {}
-					# Send a response based on return from function
-					# first arg to url[2] is the matches pulled from url
-					# second arg is unrolled data dictionary
+
+					# Send a response based on return from function first arg to
+					# url[2] is the matches pulled from url second arg is
+					# unrolled data dictionary
 					if not invalid_JSON:
 						self.respond(*url[2](match.groupdict(), **data))
 					# We found a URL, used for 404s later
@@ -69,6 +77,7 @@ class MMHandler(BaseHTTPRequestHandler):
 				else:
 					matched_url = True
 					invalid_method = True
+
 		# Error Handling Below
 		# no url match found, send 404
 		if not matched_url:
@@ -80,11 +89,15 @@ class MMHandler(BaseHTTPRequestHandler):
 			self.send_error(400)
 
 	def do_GET(self):
-		"""Handle all GET requests here by parsing URLs and mapping them to the API calls."""
+		"""Handle all GET requests.
+		
+		On GET request, parse URLs and map them to the API calls."""
 		self.match_path("GET")
 
 	def do_POST(self):
-		"""Handle all POST requests here by parsing URLs and mapping them to the API calls."""
+		"""Handle all POST requests.
+		
+		On POST request, parse URLs and map them to the API calls."""
 		self.match_path("POST")
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
