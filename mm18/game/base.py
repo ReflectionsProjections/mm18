7b5441af04187488107f5bd9ed39a76961a63534
Clare Curtis <cfcurti2@illinois.edu>
2012-05-17 22:27:46 -0500

    Added a base class w/ some comments

---
+++ mm18/game/base.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+# This is the base class.
+# It is where the code for the base goes.
+# Bases need:
+#   -Health
+#   -Resources
+#   -To create offensive/defensive units
+
+class Base:
+
+# OH NO IT'S EMPTY!
