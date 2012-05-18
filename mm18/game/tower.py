20ac4b7461d6b74d4ae73c375a283b4a613ebf5f
Clare Curtis <cfcurti2@illinois.edu>
2012-05-18 00:30:52 -0500

    Added tower class

---
+++ mm18/game/tower.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+
+# This is the tower class
+# Towers get created here.
+# I'm thinking have this as a superclass and make the specialised classes from this?
+# Or not - it may just be easier to put it all in here because towers don't need a lot
+# All towers need:
+#   -A position
+#   -Attack Power
+#   -Tower 'level'
+#   -Tower upgrade/build cost
+#   -Tower sellback price (are we implementing this?)
+
+class Tower:
+
+    
