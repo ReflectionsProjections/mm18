4b0b4d8b4039c0873cbbdbabfcc8237639f80d02
Clare Curtis <cfcurti2@illinois.edu>
2012-05-22 17:27:51 -0500

    Added more constants and added an __init__ to tower

--- mm18/game/constants.py
+++ mm18/game/constants.py
@@ -2,20 +2,29 @@
 
 #import json
 
-#TOWERS & UNITS
-BASE_DAMAGE = 10
+"""TOWERS & UNITS"""
+BASE_DAMAGE = 1 #this is currently for both towers and units.  May need to create separate ones for towers and units.
 
-#multiplier constants for when towers/units are upgraded
+"""Costs"""
+TOWER_BASE_COST = 1
+UNIT_BASE_COST = 1
+TOWER_UPGRADE_ONE_COST
+
+"""
+Multiplier constants for when towers/units are upgraded.
+Currently being used for damage multiplier and cost multiplier due to upgrades
+"""
+UPGRADE_ZERO = 1
 UPGRADE_ONE = 1
-UPGRADE_TWO = 2
-UPGRADE_THREE = 3
+UPGRADE_TWO = 1
+UPGRADE_THREE = 1
 
-#tower/unit specialisation output damage multiplier
+"""tower/unit specialisation output damage multiplier"""
 SUPER_EFFECTIVE = 1.25
 EFFECTIVE = 1.1
 NORMAL = 1
 NOT_EFFECTIVE = 0.75
 
 
-#BOARD
-BASE_HEALTH = 1000
+"""BOARD"""
+BASE_HEALTH = 1
--- mm18/game/tower.py
+++ mm18/game/tower.py
@@ -1,16 +1,39 @@
 #! /usr/bin/env python
 
-# This is the tower class
-# Towers get created here.
-# I'm thinking have this as a superclass and make the specialised classes from this?
-# Or not - it may just be easier to put it all in here because towers don't need a lot
-# All towers need:
-#   -A position
-#   -Attack Power
-#   -Tower 'level'
-#   -Tower upgrade/build cost
-#   -Tower sellback price (are we implementing this?)
+"""
+This is the tower class
+Towers get created here.
+I'm thinking have this as a superclass and make the specialised classes from this?
+Or not - it may just be easier to put it all in here because towers don't need a lot
+All towers need:
+    -A position
+    -A specialisation (or lack)
+    -Tower upgrade level
+
+Class contents:
+    -init
+    -upgrad
+"""
 
 class Tower:
 
-    
+    """
+    Creates a new Tower at grid positions X and Y.
+    All towers start with 0 upgrades and no specialisation.
+
+    @param positionX: The X position of the tower
+    @param positionY: The Y position of the tower
+    """
+    def __init__ (self, positionX, positionY)
+        self.positionX = positionX
+        self.positionY = positionY
+        self.upgrade = 0
+        self.upgradeCost = 1
+        self.specialisation = 0
+
+    """
+    Upgrades the tower.
+    This is assuming that checking whether an upgrade is allowed is done elsewhere.
+    """
+   # def upgrade(self)
+        
