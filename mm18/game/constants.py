c0749f1f6fc8cc6bdecdb1c6682648068049b1c8
Clare Curtis <cfcurti2@illinois.edu>
2012-05-22 13:00:56 -0500

    added constants file w/some constants

---
+++ mm18/game/constants.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+
+#import json
+
+#TOWERS & UNITS
+BASE_DAMAGE = 10
+
+#multiplier constants for when towers/units are upgraded
+UPGRADE_ONE = 1
+UPGRADE_TWO = 2
+UPGRADE_THREE = 3
+
+#tower/unit specialisation output damage multiplier
+SUPER_EFFECTIVE = 1.25
+EFFECTIVE = 1.1
+NORMAL = 1
+NOT_EFFECTIVE = 0.75
+
+
+#BOARD
+BASE_HEALTH = 1000
