bde5eb91e15a740f3a8c03609c6c0731ede11db0
Steven Sheldon <steven@sasheldon.com>
2012-09-08 13:59:56 -0500

    Added visualizer module.

---
+++ mm18/visualizer/visualizer.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+import pyglet
+from pyglet.gl import *
+
+class Visualizer:
+	def __init__(self):
+		self.window = pyglet.window.Window()
+		self.window.set_handler('on_draw', self.onDraw)
+		glClearColor(1, 1, 1, 1)
+
+	def onDraw(self):
+		glClear(GL_COLOR_BUFFER_BIT)
+
+	def run(self):
+		pyglet.app.run()
+
+if __name__ == '__main__':
+	viz = Visualizer()
+	viz.run()
