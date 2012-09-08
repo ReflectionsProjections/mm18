7933938cf01c243319b2c0ba112ac89833ff6d61
Nick Jeffrey <nickljeffrey@gmail.com>
2012-09-08 16:11:35 -0500

    drawing path

--- mm18/visualizer/visualizer.py
+++ mm18/visualizer/visualizer.py
@@ -1,8 +1,10 @@
 import os
 import pyglet
 from pyglet.gl import *
+from pyglet import image
 
 from mm18.game.board import Board
+from mm18.game.constants import BOARD_SIDE
 
 class Visualizer:
 	resourcesPath = os.path.join(os.path.dirname(__file__), 'resources')
@@ -14,12 +16,18 @@ class Visualizer:
 
 	def __init__(self, board):
 		self.board = board
-		self.window = pyglet.window.Window()
+		self.window = pyglet.window.Window(32*BOARD_SIDE, 32*BOARD_SIDE)
 		self.window.set_handler('on_draw', self.onDraw)
 		glClearColor(1, 1, 1, 1)
 
 	def onDraw(self):
 		self.window.clear()
+		for x in range(BOARD_SIDE):
+			for y in range(BOARD_SIDE):
+				self.texTerrain.blit(x*32,y*32)
+		path=self.board.path
+		for block in path:
+			self.texPath.blit(block[0]*32, block[1]*32)
 
 	def run(self):
 		pyglet.app.run()
