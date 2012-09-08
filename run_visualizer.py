5bc6ec4cb6cf19d76e68b583ba3688b83d73c2f8
Steven Sheldon <steven@sasheldon.com>
2012-09-08 18:22:24 -0500

    Visualizer Board is loaded from JSON.

--- run_visualizer.py
+++ run_visualizer.py
@@ -3,7 +3,6 @@
 from mm18.visualizer.visualizer import Visualizer
 from mm18.game.board import Board
 
-path = [(0, 0), (1, 1), (3, 4)]
-board = Board([], path)
+board = Board.jsonLoad()
 viz = Visualizer(board)
 viz.run()
