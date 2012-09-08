e9cff9d0f344c6cd9f0b1d293203e380bc539f6d
Steven Sheldon <steven@sasheldon.com>
2012-09-08 15:51:15 -0500

    Added resourcePath member to Visualizer.

--- mm18/visualizer/visualizer.py
+++ mm18/visualizer/visualizer.py
@@ -1,9 +1,17 @@
+import os
 import pyglet
 from pyglet.gl import *
 
 from mm18.game.board import Board
 
 class Visualizer:
+	resourcesPath = os.path.join(os.path.dirname(__file__), 'resources')
+	pyglet.resource.path.append(resourcesPath)
+	pyglet.resource.reindex()
+
+	texTerrain = pyglet.resource.image('genericGrass.png')
+	texPath = pyglet.resource.image('genericPath.png')
+
 	def __init__(self, board):
 		self.board = board
 		self.window = pyglet.window.Window()
