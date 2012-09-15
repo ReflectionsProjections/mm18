2555f8d3c5defd8bce63b05af0ad09b0f359f29d
Josh Ginsburg <ginsbur3@illinois.edu>
2012-09-15 18:03:58 -0500

    temp gameengine and change to constants

--- mm18/game/constants.py
+++ mm18/game/constants.py
@@ -3,16 +3,22 @@
 #import json
 #this class needs organisation
 
-"""TOWERS & UNITS"""
-BASE_DAMAGE = 1 #this is currently for both towers and units.  May need to create separate ones for towers and units.
+"""TOWERS"""
+BASE_TOWER_DAMAGE = 1
 MAX_UPGRADE = 3 #max upgrade level for towers and units
-BASE_UNIT_HEALTH = 1
 TOWER_SELL_SCALAR = 1
 TOWER_BASE_COST = 1
 TOWER_RANGE = {0:1, 1:1, 2:2}
+TOWER_UPGRADE_MULTIPLIER = {0:1, 1:1.5, 2:2.5, 3:3.5}
+TOWER_UPGRADE_COST = {1:1, 2:2, 3:3}
+
+"""UNITS"""
 UNIT_BASE_COST = 1
-UPGRADE_MULTIPLIER = {0:1, 1:1.5, 2:2.5, 3:3.5}
-UPGRADE_COST = {1:1, 2:2, 3:3}
+BASE_UNIT_HEALTH = 1
+BASE_UNIT_DAMAGE = 1
+UNIT_UPGRADE_MULTIPLIER = {0:1, 1:1.5, 2:2.5, 3:3.5}
+UNIT_UPGRADE_COST = {1:1, 2:2, 3:3}
+
 
 """tower/unit specialisation output damage multiplier"""
 SUPER_EFFECTIVE = 1.25
@@ -33,3 +39,6 @@ BASE_SIZE = 3
 
 """PLACEHOLDER"""
 BOOP = -1000
+
+"""MODEL"""
+SUPPLY_TIME = 4;
--- mm18/game/engine.py
+++ mm18/game/engine.py
@@ -1,13 +1,64 @@
+#! /usr/bin/env python
+
+import mm18.game.constants
+from mm18.game.board import Board
+from mm18.game.player import Player
+
 class Engine():
 
 	def __init__(self):
-		pass
+
+		#generate players and boards
+		self.players = []
+		for i in range(0-4)
+			self.players.append(Player(i))
+		self.boards = []
+		for i in range(0-4)
+			self.boards.append(Board([(0,1),(1,1)],[(0,2),(1,2),(1,3),(0,4)]))
+		self.maxtier = 0
+		self.numDead = 0
+		self.curTick = 0
 
 	# Game controls
 
 	def advance(self):
+		self.curTick += 1;		
+		if self.curTick%constants.SUPPLY_TIME == 0:
+			supply() 
+		moveUnits() 
+		countDead() 
+		towerResponces()
+		if self.numDead == 3 :
+			endGame()	
+
+	def supply(self):
+		for i in self.get_player_ids():
+				if self.get_player(i).self.get_player(i).allowedUpgradeIs() > self.maxtier :
+					self.maxtier = self.get_player(i).allowedUpgradeIs()
+
+		for i in self.get_player_ids():
+				self.get_player(i).addResource(BASE_RESOURCES + UPGRADE_INCREASE*self.maxtier)
+	
+	def moveUnits(self):
+		# moveunits on all valid boards
+		#for all players : player.board.moveunits
+		pass
+	
+	def countDead(self):
+		count = 0
+		for i in self.get_player_ids():
+			if self.get_player(i).isDead(): count += 1
+		if count > self.numDead: self.numDead = count
+	
+	def towerResponces(self):
+		# all towers attack
+		#for all players : player.board.useTowers
 		pass
 
+	def endGame(self):
+		# endGamecondition
+		pass
+	
 	# Player controls
 
 	def get_player_ids(self):
