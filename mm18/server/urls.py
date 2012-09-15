ef822ec1f04fe84f0abe4ceb738c268218bab188
Dylan Nugent <dylnuge@gmail.com>
2012-09-15 16:27:25 -0500

    Stubbing out API control methods
    urls.py now contains the mapping paths for every currently expected API
    functionality. game_controller.py now contains a set of functions for
    operating on a game engine given a regex match object and a list of
    keyworded arguments from JSON; these functions are all stubs at this
    time.

--- mm18/server/urls.py
+++ mm18/server/urls.py
@@ -1,8 +1,26 @@
 """URL Patterns for mapping URLs to the appropriate functions."""
 
-from mm18.server.server_test_api import *
+from mm18.game.game_controller import *
 
 urlpatterns = [
-	(r'/api/tests/echo/(?P<code>\d+)', 'GET', echo_code),
-	(r'/api/tests/post', 'POST', POST_method),
+	# Commands for overall game
+	(r'/game/status', 'GET', get_game_status),
+
+	# Commands for player control, status, etc
+	(r'/player/(?P<id>\d+)', 'GET', get_player_status),
+
+	# Commands for retrieving representation details
+	(r'/board/(?P<id>\d+)', 'GET', board_get),
+
+	# Tower API
+	(r'/tower/(?P<id>\d+)/upgrade', 'POST', tower_uprade),
+	(r'/tower/(?P<id>\d+)/specialize', 'POST', tower_specialize),
+	(r'/tower/(?P<id>\d+)/sell', 'POST', tower_sell),
+	(r'/tower/(?P<id>\d+)', 'GET', tower_get),
+	(r'/tower/create', 'POST', tower_create),
+	(r'/tower', 'GET', tower_list),
+
+	# Unit API
+	(r'/unit/(?P<id>\d+)', 'GET', unit_status),
+	(r'/unit/create', 'POST', unit_create),
 ]
