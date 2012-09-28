246ba2a0833939ec90985cb24314bae006bbe053
Steven Sheldon <steven@sasheldon.com>
2012-09-28 15:13:23 -0500

    Updating run_visualizer to compile.
    Who thought having stable method signatures was a good idea, anyways?

--- run_visualizer.py
+++ run_visualizer.py
@@ -3,6 +3,6 @@
 from mm18.visualizer.visualizer import Visualizer
 from mm18.game.board import Board
 
-board = Board.jsonLoad()
+board = Board.jsonLoad('board1.json')
 viz = Visualizer(board)
 viz.run()
