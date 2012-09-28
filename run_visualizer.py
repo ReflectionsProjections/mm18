d21e5a49f15c1e0cda7b6d7fa816169f1ad6a309
Steven Sheldon <steven@sasheldon.com>
2012-09-28 17:42:24 -0500

    run_visualizer script uses an Engine.

--- run_visualizer.py
+++ run_visualizer.py
@@ -1,8 +1,15 @@
 #! /usr/bin/env python
 
 from mm18.visualizer.visualizer import Visualizer
-from mm18.game.board import Board
+from mm18.game.engine import Engine
 
-board = Board.jsonLoad('board1.json')
-viz = Visualizer(board)
+game = Engine()
+game.add_player('p1')
+game.tower_create(
+	owner_id='p1',
+	coords=(0, 2),
+	level=1,
+	spec=0
+)
+viz = Visualizer(game.board_get('p1'))
 viz.run()
