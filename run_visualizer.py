4bd5d110de3ff6b148aa7fac50aa315419a694e4
Steven Sheldon <steven@sasheldon.com>
2012-09-28 22:12:32 -0500

    Updated run_visualizer to use Engine's unit_create.

--- run_visualizer.py
+++ run_visualizer.py
@@ -8,9 +8,6 @@ from mm18.game import constants
 game = Engine()
 game.add_player('p1')
 game.tower_create('p1', (0, 2))
-game.board_get('p1').queueUnit(
-	Unit(1, 0, game.get_player('p1')),
-	constants.NORTH
-)
+game.unit_create('p1', 1, 0, 'p1', constants.NORTH)
 viz = Visualizer(game)
 viz.run()
