932add8be668b057f9f98cc7ca9f8a388ee93e68
Steven Sheldon <steven@sasheldon.com>
2012-09-28 21:10:41 -0500

    run_visualizer script starts a unit on a path.

--- run_visualizer.py
+++ run_visualizer.py
@@ -2,9 +2,14 @@
 
 from mm18.visualizer.visualizer import Visualizer
 from mm18.game.engine import Engine
+from mm18.game.units import Unit
+from mm18.game import constants
 
 game = Engine()
 game.add_player('p1')
 game.tower_create('p1', (0, 2))
+path = game.board_get('p1').paths[constants.NORTH]
+unit = Unit(1, 0, game.get_player('p1'), None)
+path.start(unit)
 viz = Visualizer(game)
 viz.run()
