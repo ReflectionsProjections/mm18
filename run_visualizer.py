128b3b193f30ed8447486d0c6e469383d4fe41dc
Steven Sheldon <steven@sasheldon.com>
2012-10-05 20:48:30 -0500

    Visualizer run script uses argparse.

--- run_visualizer.py
+++ run_visualizer.py
@@ -1,8 +1,24 @@
 #! /usr/bin/env python
 
+import sys
+import argparse
+
 from mm18.visualizer.visualizer import Visualizer
 
-with open('mm18/mocks/game_log.json') as f:
-	lines = f.readlines()
-viz = Visualizer(iter(lines))
-viz.run()
+def main():
+	parser = argparse.ArgumentParser(
+		description='Visualizes MechMania 18 games.')
+	parser.add_argument('LOG', type=argparse.FileType('r'),
+		nargs='?', default='mm18/mocks/game_log.json',
+		help='Log file to replay game from')
+	parser.add_argument('PLAYER', nargs='?', default='p1',
+		help='Player whose Board should be shown')
+	args = parser.parse_args()
+
+	lines = args.LOG.readlines()
+	args.LOG.close()
+	viz = Visualizer(iter(lines))
+	viz.run()
+
+if __name__ == "__main__":
+	sys.exit(main())
